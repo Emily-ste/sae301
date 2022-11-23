734ffdca2d1824f18c3ac781e61d521dcea811d3
Github MMI3 <github@mmi-troyes.fr>
2022-11-23 07:52:20 +0000

    creation des controllers

---
+++ src/Controller/ValidCommController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ValidCommController extends AbstractController
+{
+    #[Route('/valid/comm', name: 'app_valid_comm')]
+    public function index(): Response
+    {
+        return $this->render('valid_comm/index.html.twig', [
+            'controller_name' => 'ValidCommController',
+        ]);
+    }
+}
