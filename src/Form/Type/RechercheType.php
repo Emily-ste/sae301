d567ba7aca9c5a503225f66fd755a3b46cb319e0
root <root@mmi21d05.mmi-troyes.fr>
2022-11-28 15:17:08 +0100

    Forms 1/? - Form rudimentaire fini

--- src/Form/Type/RechercheType.php
+++ src/Form/Type/RechercheType.php
@@ -3,25 +3,23 @@
 namespace App\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
-use App\Entity\Task;
+use App\Entity\Recherche;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class TaskType extends AbstractType{
+class RechercheType extends AbstractType{
     public function buildForm(FormBuilderInterface $builder, array $options): void{
         $builder
-            ->add('task', TextType::class)
-            ->add('dueDate', DateType::class)
+            ->add('recherche', TextType::class)
             ->add('save', SubmitType::class)
         ;
     }
     public function configureOptions(OptionsResolver $resolver): void{
         $resolver->setDefaults([
-            'data_class' => Task::class,
+            'data_class' => Recherche::class,
         ]);
     }
 }
