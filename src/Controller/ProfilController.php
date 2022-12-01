5250d5cedcd49966e2e842ba527601ae2a3337b9
emyx2077 <emily.saintetienne@gmail.com>
2022-12-01 09:14:40 +0100

    edit infos

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Clients;
+use App\Form\ClientType;
 use App\Repository\ClientsRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -23,25 +26,41 @@ class ProfilController extends AbstractController
     }
 
     #[Route('/profil/edit', name: 'app_client_edit')]
-    public function edit(ClientsRepository $clientsRepository): Response
+    public function edit(ClientsRepository $clientsRepository, Request $request): Response
     {
+        //get id of current user
         $id = $this->getUser()->getId();
         $client = $clientsRepository->findUserById($id);
+
+        //create form with current user data
+        $form = $this->createForm(ClientType::class, $client);
+        $form->remove('client_adr_rue');
+        $form->remove('client_adr_ville');
+        $form->remove('client_adr_cp');
+        $form = $form->handleRequest($request);
+
         return $this->render('profil/client_edit.html.twig', [
-            'client' => $client,
-            'controller_name' => 'ProfilController',
+            'form' => $form->createView(),
         ]);
     }
 
     //edit adresse
     #[Route('/profil/edit/adresse', name: 'app_client_edit_adresse')]
-    public function editAdresse(ClientsRepository $clientsRepository): Response
+    public function editAdresse(ClientsRepository $clientsRepository, Request $request): Response
     {
+        //get id of current user
         $id = $this->getUser()->getId();
         $client = $clientsRepository->findUserById($id);
+
+        //create form with current user data
+        $form = $this->createForm(ClientType::class, $client);
+        $form->remove('email');
+        $form->remove('client_nom');
+        $form->remove('client_prenom');
+        $form = $form->handleRequest($request);
+
         return $this->render('profil/client_edit_adresse.html.twig', [
-            'client' => $client,
-            'controller_name' => 'ProfilController',
+            'form' => $form->createView(),
         ]);
     }
 
