6b6c8fcb4a7ddb939ab15e2fed7c247a91bc32a3
emyx2077 <emily.saintetienne@gmail.com>
2022-12-02 14:36:14 +0100

    form contact (front)

--- src/Controller/AutresController.php
+++ src/Controller/AutresController.php
@@ -2,17 +2,33 @@
 
 namespace App\Controller;
 
+use App\Form\ClientType;
+use App\Form\ContactType;
+use App\Repository\ClientsRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AutresController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function contact(): Response
+    public function contact(ManagerRegistry $doctrine, ClientsRepository $clientsRepository, Request $request): Response
     {
+
+        //create form with current user data
+        $form = $this->createForm(ContactType::class);
+        $form = $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            //get data
+            $contact = $form->getData();
+            return $this->redirectToRoute('app_contact');
+        }
+
         return $this->render('autres/contact.twig', [
-            'controller_name' => 'ContactController',
+            'form' => $form->createView(),
         ]);
     }
 
