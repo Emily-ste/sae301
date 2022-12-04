aba3a333ac147cb854f6a8d1f1684beccc046d64
emyx2077 <emily.saintetienne@gmail.com>
2022-12-04 01:31:03 +0100

    debut back

--- src/Controller/CommandeController.php
+++ src/Controller/CommandeController.php
@@ -3,15 +3,24 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CommandeController extends AbstractController
 {
     #[Route('/commande', name: 'app_commande')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        //get cookie
+        $cookie = $request->cookies->get('panier');
+
+        $user = $this->getUser();
+
+
+        //delete session cookie
         return $this->render('commande/index.html.twig', [
+            'cookie' => $cookie,
             'controller_name' => 'CommandeController',
         ]);
     }
